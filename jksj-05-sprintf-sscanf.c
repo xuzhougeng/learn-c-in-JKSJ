def76b31054f78e7e9b09a3e94042c6d548db389
xuzhougeng <xuzhougeng@yeah.net>
2020-01-16 10:50:02 +0800

    极客时间:人人都能学会的编程入门课，第6讲

---
+++ jksj-05-sprintf-sscanf.c
@@ -0,0 +1,34 @@
+#include <stdio.h>                                  
+/* 题目                                             
+ * 输入一个小数和保留位数                           
+ * 输出保留后的小数                                 
+ */                                                 
+
+/* 知识点:
+ * 1. 格式输入中, 为了表示一个%, 需要用%%
+ * 2. sprintf: 将原本输出到屏幕的内容，保存到变量中
+ * 3. sscanf: 将原本从屏幕接受的字符串输入，从字符串数组中读取
+ * 4. 字符串数组需要以'\0'表示字符串的结束
+ */
+
+
+int main()                                          
+{                                                   
+    // how many digit to keep                       
+    int n;                                          
+    // input number                                 
+    double num;                                     
+                                                    
+    // character array to store input               
+    char str[100];                                  
+                                                    
+    printf("Please enter your number: ");           
+    scanf("%lf", &num);                             
+    printf("How many digits do you want to keep: ");
+    scanf("%d", &n);                                
+                                                    
+    // format the str to num                        
+    sprintf(str,"%%.%dlf\n", n);                    
+    printf(str, num);                               
+                                                    
+}                                                   
